8db5e873086b857d3223256d5103696752fa5af5
Botdeveloper358 <indonesiyaserver852101@gmail.com>
2025-07-16 01:20:08 +0530

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBS_o8VFm-axOfWS61hwkBTZcg1JMonpJo",
+    authDomain: "weeshmaster-portfolio.firebaseapp.com",
+    projectId: "weeshmaster-portfolio",
+    storageBucket: "weeshmaster-portfolio.appspot.com",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abcdef123456"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
